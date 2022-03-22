55563f8a00e1d028ed5f4c06090248cfdb4ad5fc
Thottyottyotty <thot@thiic.cc>
2022-03-21 21:00:20 -0700

    basic webXR functionality

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ import * as THREE from './three/three.module.js';
 import { GLTFLoader } from './three/GLTFLoader.js';
 import { DRACOLoader } from './three/DRACOLoader.js';
 import { OrbitControls } from './three/OrbitControls.js';
+import { VRButton } from './three/webxr/VRButton.js';
 
 window.THREE = THREE
 
 let camera, scene, renderer, gamepad;
-let controls
+let controls, dummy, controller
 let geometry, material, mesh;
+let startpos, prepos
 
 //buttons
 //0  1  2  3  4  5  6  7
@@ -23,6 +25,7 @@ function init() {
 
   camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 20000 );
   camera.position.z = 1;
+
 
 
   scene = new THREE.Scene();
@@ -49,36 +52,86 @@ function init() {
     //gltf.scene.position.set(0,-6,-19)
 
     scene.add( gltf.scene );
-  camera.position.x = -5
-camera.position.y = 1.7
-camera.position.z = 6
-let bm = new THREE.MeshBasicMaterial()
-bm.map = scene.children[1].children[0].children[1].material.map
-    bm.map.encoding = 3000
-scene.children[1].children[0].children[1].material = bm
-let bm2 = new THREE.MeshBasicMaterial()
-bm2.map = scene.children[1].children[0].children[0].material.map
+    camera.position.x = -5
+    camera.position.y = 1.7
+    camera.position.z = 6
+    let bm2 = new THREE.MeshBasicMaterial()
+    bm2.map = scene.children[1].children[0].material.map
     bm2.map.encoding = 3000
-scene.children[1].children[0].children[0].material = bm2
-camera.rotateY(-0.7)
+    scene.children[1].children[0].material = bm2
+    camera.rotateY(-0.7)
+    document.body.appendChild( VRButton.createButton( renderer ) );
   })
 
-    //camera.add( gltf.scene );
+  //camera.add( gltf.scene );
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setAnimationLoop( animation );
-    document.body.appendChild( renderer.domElement );
-    controls = new OrbitControls (camera, renderer.domElement);
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setAnimationLoop( animation );
+  renderer.xr.enabled = true;
+  renderer.xr.addEventListener('sessionstart', function () {
 
-    Object.assign(window, {camera, scene,renderer})
 
-  }
+    scene.remove(dummy)
+    dummy = new THREE.Object3D()
+    dummy.position.x = -5
+    dummy.position.z = 6
+    dummy.add(camera)
+    scene.add(dummy)
+    function onSelectStart() {
+      startpos = {...controller.position}
+      prepos = {...dummy.position}
+
+      this.userData.isSelecting = true;
+
+    }
+
+    function onSelectEnd() {
+
+      this.userData.isSelecting = false;
+
+    }
+
+
+
+    controller = renderer.xr.getController( 0 );
+    controller.addEventListener( 'selectstart', onSelectStart );
+    controller.addEventListener( 'selectend', onSelectEnd );
+    /*
+    controller.addEventListener( 'connected', function ( event ) {
+
+      this.add( buildController( event.data ) );
+
+    } );
+    controller.addEventListener( 'disconnected', function () {
+
+      this.remove( this.children[ 0 ] );
+
+    } );
+    */
+    scene.add( controller );
+  });
+  renderer.xr.addEventListener('sessionend', function () {
+    scene.add(camera)
+    scene.remove(dummy)
+  });
+  document.body.appendChild( renderer.domElement );
+  controls = new OrbitControls (camera, renderer.domElement);
+
+  Object.assign(window, {camera, scene,renderer})
+
+}
 
 let speed = 0
 
 function animation( time ) {
   controls.update();
+  if ( controller?.userData?.isSelecting === true ) {
+    dummy.position.x = prepos.x - (controller.position.x - startpos.x)*2
+    dummy.position.z = prepos.z - (controller.position.z - startpos.z)*2
+    dummy.position.y = prepos.y - (controller.position.y - startpos.y)*2
+    console.log(controller)
+  }
   /*
   let gamepad = navigator.getGamepads()[0]
   if (speed > 0.7) speed = 100.7
